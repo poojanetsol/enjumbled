92bc7369b0bc829c79d028b97298057355fff305
pooja1133 <pooja.goyal@netsolutions.com>
2018-07-24 13:28:45 +0530

    first commit

---
+++ src/app/app.routing.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RoutingTable } from './routing.config';
+
+const appRoutes: Routes = RoutingTable;
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule {}
